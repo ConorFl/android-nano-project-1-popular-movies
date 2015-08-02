48bcf6502e56e25c0169c5972d4802fcccffa6ec
ConorFl <conoreflanagan@gmail.com>
2015-08-01 18:01:46 -0700

    settings activity added, removed some shit for SettingsActivity (see http://stackoverflow.com/questions/15854225/java-lang-nullpointerexception-due-to-setupactionbar-in-android), about to gut all this placeholder stuff in settings

--- app/src/main/java/com/example/android/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/SettingsActivity.java
@@ -45,18 +45,6 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setupActionBar();
-    }
-
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void setupActionBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            // Show the Up button in the action bar.
-            getActionBar().setDisplayHomeAsUpEnabled(true);
-        }
     }
 
     @Override
