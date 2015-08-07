44be1adb861f5b212b6014a6f39cb98f5725edc4
ConorFl <conoreflanagan@gmail.com>
2015-08-06 21:58:46 -0700

    implement parceable in movie class

--- app/src/main/java/com/example/android/popularmovies/Movie.java
+++ app/src/main/java/com/example/android/popularmovies/Movie.java
@@ -1,9 +1,12 @@
 package com.example.android.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by conorflanagan on 7/28/15.
  */
-public class Movie {
+public class Movie implements Parcelable {
     int id;
     String title;
     String overview;
@@ -15,4 +18,37 @@ public class Movie {
         this.overview = overview;
         this.imageUrl = imageUrl;
     }
+
+    private Movie(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        overview = in.readString();
+        imageUrl = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeString(overview);
+        dest.writeString(imageUrl);
+    }
+
+    public final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
